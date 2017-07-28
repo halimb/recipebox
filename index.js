f3661ee897a718789d5f60ec2c3c487776ece400
Halim <halim.bouguedra@gmail.com>
2017-07-28 12:41:24 +0100

    center main app div and split the rest into tiles

--- index.js
+++ index.js
@@ -1,29 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { RecipeForm } from "./components/RecipeForm";
+import { MainContainer } from "./components/MainContainer";
 import "./styles/style.scss";
 
-class MainContainer extends React.Component {
+class App extends React.Component {
 	render() {
 		return (
-				<div className="main">
-					<div className="tile">
-						<div className="tile is-parent is-vertical is-2">
-								<div className="tile box is-primary is-child">foo</div>
-								<div className="tile notification is-primary is-child">bar</div>
-						</div>
-						<div className="tile is-parent">
-							<div className="tile is-child">
-								<RecipeForm/>
-							</div>
-						</div>
-					</div>
+				<div className="main box">
+					<MainContainer/>
 				</div>
 			)
 	}
 }
 
 ReactDOM.render(
-	<MainContainer></MainContainer>,
+	<App/>	,
 	document.getElementById("app")
 )
