ad20210cefb909a90e278ce247720e9e5d9fff44
Halim <halim.bouguedra@gmail.com>
2017-07-28 22:47:31 +0100

    merge IngredientsBox component into RecipeForm inorder to overcome upstream data flow limitations and issues

--- components/RecipeForm.js
+++ components/RecipeForm.js
@@ -1,63 +1,86 @@
 import React from "react"
-import { IngredientsBox } from "./IngredientsBox"
 
 export class RecipeForm extends React.Component {
 	constructor(props) {
 		super(props)
 		this.state = {
 						name: "",
+						ingr: "",
 						nameMsg: "",
 						nameClass: "",
 						ingMsg: "",
 						ingClass: "",
+						maxIngMsg: "",
+						maxIngClass: "",
 						ingredients: [],
-						active: true,
-						submit: false
+						submit: false,
+						disabled: false
 					}
 
-		this.clear = this.clear.bind(this)
 		this.submit = this.submit.bind(this)
-		this.activate = this.activate.bind(this)
 		this.onNameChange = this.onNameChange.bind(this)
 		this.submitRecipe = this.submitRecipe.bind(this)
-		this.getIngredients = this.getIngredients.bind(this)
+		this.addIngredient = this.addIngredient.bind(this)
+		this.handleIngr = this.handleIngr.bind(this)
+		this.reset = this.reset.bind(this)
 	}
 
-	clear() {
+	reset() {
 		this.setState({
+						ingr: "",
 						name: "",
 						nameMsg: "",
 						nameClass: "",
 						ingMsg: "",
 						ingClass: "",
 						ingredients: [],
-						active: false,
 						submit: false,
-						preSubmit: false
+						maxIngMsg: "",
+						maxIngClass: "",
+						disabled: false
 					})
 	}
 
-	activate() {
-		this.setState({active: true})
-	}
-
 	onNameChange(changeEvent) {
-		this.setState({name: changeEvent.target.value});
+		this.setState({name: changeEvent.target.value})
 	}
 
-	getIngredients(ingr) {
-		this.setState({
-			ingredients: ingr,
-			ingMsg: "",
-			ingClass: ""
-		},
+	handleIngr(event) {
+		this.setState({ingr: event.target.value})
+	}
 
-		() => 	{
-					if(this.state.preSubmit) {
-						 this.submitRecipe() 
-					}
-				}
-		);
+	addIngredient(e) {
+		if(e) { e.preventDefault() }
+		let newIngr = this.state.ingr
+		if(newIngr != "") {
+			let ingredients = this.state.ingredients
+			// Test for max number of ingredients
+			if(ingredients.length > 12) {
+				this.setState({
+								maxIngMsg: "seems like enough ingredients!",
+								maxIngClass: " is-danger",
+								disabled: true,
+								ingr: ""
+							})
+			}
+			else {
+				newIngr = (<span className="tag is-light margin" 
+								key={ingredients.length}>
+								{newIngr}
+							</span>)
+				ingredients.push(<span className="tag is-light margin" 
+										key={ingredients.length}>
+										{newIngr}
+								 </span>)
+				this.setState({
+							ingr: "",
+							ingredients: ingredients,
+							maxIngMsg: "",
+							maxIngClass: "",
+							disabled: false
+						})
+			}
+		}
 	}
 
 	submitRecipe() {
@@ -67,13 +90,13 @@ export class RecipeForm extends React.Component {
 						name,
 						ingr 
 					  }
-		this.props.addRecipe(recipe);
-		this.clear();
+		this.props.addRecipe(recipe)
+		this.reset()
 	}
 
 	submit() {
+		this.addIngredient()
 		this.setState({
-				preSubmit: true,
 				submit: true,
 				nameMsg: "",
 				nameClass: "",
@@ -91,27 +114,21 @@ export class RecipeForm extends React.Component {
 				this.submitRecipe()
 			}
 
+			//Test for required fields
 			else {
 				if(nameLen == 0) {
 					this.setState({
-									nameClass: " is-danger",
-									nameMsg: "* a good recipe must have name!",
-									preSubmit: false,
-									submit: false
-								})
+								nameClass: " is-danger",
+								nameMsg: "* a good recipe must have name!",
+								submit: false
+							})
 				}
 				if(ingLen == 0) {
-							this.setState({
-										ingClass: " ingBox",
-										ingMsg: "* a good recipe must have ingredients!",
-										submit: false
-									})
-					window.setTimeout(()=>
-					{
-						if(this.state.preSubmit){
-							this.setState({preSubmit: false})
-						}
-					}, 500);	
+					this.setState({
+								ingClass: " ingBox",
+								ingMsg: "* a good recipe must have ingredients!",
+								submit: false
+							})
 				}
 			}
 		}
@@ -136,10 +153,29 @@ export class RecipeForm extends React.Component {
 					</div>
 					<br/>
 					<div className={ this.state.ingClass }>
-						<IngredientsBox preSubmit={this.state.preSubmit}
-										submit={this.state.submit}
-										submitIngr={this.getIngredients}
-										active={this.state.active}/>
+						<form onSubmit={this.addIngredient} className="field box">
+							<h1>Ingredients</h1>
+							<br/>
+								{this.state.ingredients}
+							<hr/>
+							<div className="field has-addons">
+								<p className="control">
+									<input  value={this.state.ingr}
+											onChange={this.handleIngr} 
+											disabled={this.state.disabled}
+											placeholder="Add an ingredient" 
+											className={"input "+ this.state.maxIngClass}/>
+								</p>
+								<p className="control">
+									<button onClick={this.addIngredient} className="button is-primary">
+										+
+									</button>
+								</p>
+							</div>
+							<p className="help is-danger">
+								{this.state.maxIngMsg}
+							</p>
+						</form>
 					</div>
 					<p className="help is-danger">
 							{this.state.ingMsg}
@@ -153,7 +189,7 @@ export class RecipeForm extends React.Component {
 							</button>
 						</div>
 						<div className="control">
-							<button onClick={this.clear} className="button is-outlined">
+							<button onClick={this.reset} className="button is-outlined">
 								Cancel
 							</button>
 						</div>
